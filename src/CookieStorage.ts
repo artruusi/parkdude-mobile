e9fc49a3460ab0b43b240987191218655798d1b4
aatosm <aatosm@hotmail.com>
2019-10-18 20:49:50 +0300

    Added CookieStorage

---
+++ src/CookieStorage.ts
@@ -0,0 +1,28 @@
+import { AsyncStorage } from 'react-native';
+
+async function setCookie(cookie: string) {
+  try {
+    await AsyncStorage.setItem('cookie', cookie)
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+async function getCookie() {
+  try {
+    const cookie = await AsyncStorage.getItem('cookie');
+    return cookie;
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+async function removeCookie() {
+  try {
+    await AsyncStorage.removeItem('cookie');
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+export { setCookie, getCookie, removeCookie };
