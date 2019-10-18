ec3d1328c3c82a569558c3d209d5e4e4cc7dbdb5
aatosm <aatosm@hotmail.com>
2019-10-18 20:39:16 +0300

    Added auth state and typescript props interfaces

--- App.tsx
+++ App.tsx
@@ -1,21 +1,34 @@
 import React, {Component} from 'react';
-import { NativeRouter, Route, Redirect } from "react-router-native";
-import LoginView from './src/components/LoginView'
-import MainView from './src/components/MainView'
-import MyReservationsView from './src/components/MyReservationsView'
-import MakeReservationView from './src/components/MakeReservationView'
-import PrivateRoute from './src/PrivateRoute'
+import { NativeRouter, Route, Redirect } from 'react-router-native';
+import LoginView from './src/components/LoginView';
+import MainView from './src/components/MainView';
+import MyReservationsView from './src/components/MyReservationsView';
+import MakeReservationView from './src/components/MakeReservationView';
+import PrivateRoute from './src/PrivateRoute';
 
-export default class App extends Component {
+interface MainState {
+  user: string,
+  error: Object,
+  isAuthenticated: boolean
+}
+
+export default class App extends Component<{}, MainState> {
   
-  // TODO: Handle authentication
+  constructor(props: {}) {
+    super(props);
+    this.state = {
+      user: "",
+      error: {},
+      isAuthenticated: false
+    }
+  }
 
   render() {
     return (
       <NativeRouter>
-        <Redirect exact from="/" to="main" />
+        <Redirect exact from="/" to="/main" />
         <Route path="/login" component={LoginView} />
-        <PrivateRoute name="main" path="/main" component={MainView} />
+        <PrivateRoute path="/main" component={MainView} />
         <PrivateRoute path="/my_reservations" component={MyReservationsView} />
         <PrivateRoute path="/make_reservation" component={MakeReservationView} />
       </NativeRouter>
--- src/components/LoginView.tsx
+++ src/components/LoginView.tsx
@@ -1,11 +1,50 @@
-import React, {Component} from 'react';
-import {StyleSheet, View} from 'react-native';
-import Icon from 'react-native-vector-icons/FontAwesome';
+import React, { Component } from "react";
+import { StyleSheet, View } from "react-native";
+import Icon from "react-native-vector-icons/FontAwesome";
+import { AuthSession } from "expo";
+import { LOGIN_URL } from "react-native-dotenv";
+import { setCookie } from "../CookieStorage";
+import { getAuthState } from "../actions/authActions"
+import { connect } from "react-redux";
 
-export default class LoginView extends Component {
-  handleLogin() {
-    // event handler triggered by Login-button
-    // TODO: Add Login functionality
+interface Props {
+  getAuthState: () => void,
+  navigation: any
+}
+
+class LoginView extends Component<Props> {
+  constructor(props: Props) {
+    super(props);
+    this.loginGoogle = this.loginGoogle.bind(this);
+  }
+
+  async loginGoogle() {
+    try {
+      // TODO: Get more static url
+      const redirectUrl = AuthSession.getRedirectUrl();
+      console.log(redirectUrl);
+
+      const result = await AuthSession.startAsync({
+        authUrl: LOGIN_URL + `?redirectUrl=${redirectUrl}`
+      });
+
+      console.log("Returned from browser", result);
+
+      if (result.type === "success") {
+        const sessionId = result.params.sessionId;
+        await setCookie(`sessionId=${sessionId}`);
+        this.props.getAuthState();
+      }
+    } catch (error) {
+      // TODO: when will this situation happen?
+      console.log(error);
+    }
+  }
+
+  componentWillReceiveProps(receivedProps) {
+    if (receivedProps.isAuthenticated) {
+      this.props.navigation.navigate("App");
+    }
   }
 
   render() {
@@ -14,7 +53,7 @@ export default class LoginView extends Component {
         <Icon.Button
           name="google"
           backgroundColor="#DD4B39"
-          onPress={this.handleLogin}
+          onPress={this.loginGoogle}
         >
           Login with Google
         </Icon.Button>
@@ -23,11 +62,19 @@ export default class LoginView extends Component {
   }
 }
 
+const mapStateToProps = (state) => ({
+  isAuthenticated: state.auth.isAuthenticated
+});
+
+const mapDispatchToProps = { getAuthState }
+
+export default connect(mapStateToProps, mapDispatchToProps)(LoginView);
+
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center'
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center"
   }
 });
