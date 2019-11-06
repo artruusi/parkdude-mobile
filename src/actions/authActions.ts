790aadc30bf1dacf2af76b7ec5d7d86b362b29cd
aatosm <aatosm@hotmail.com>
2019-11-06 11:57:03 +0200

    Forgot to add HttpMethod to logout action creator, fixed.

--- src/actions/authActions.ts
+++ src/actions/authActions.ts
@@ -32,7 +32,7 @@ export const logOut = () => {
   return async (dispatch) => {
     try {
       // TODO: if GET /auth/logout is deprecated, change to POST
-      const authResponse = await apiFetch(HttpMethod.GET, LOGOUT_URL);
+      const authResponse = await apiFetch(LOGOUT_URL, {method: HttpMethod.GET});
       const result = await authResponse.json();
       await removeCookie();
       dispatch(setLogOutState(result));
