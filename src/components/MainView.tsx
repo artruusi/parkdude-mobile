0d274416f5f8e492cc5b6d5c5fefc942e7bcefe5
aatosm <aatosm@hotmail.com>
2019-11-14 18:24:03 +0200

    Implemented simple date selection

--- src/components/MainView.tsx
+++ src/components/MainView.tsx
@@ -1,22 +1,58 @@
 import React, {Component} from 'react';
 import {StyleSheet, Text, View, TouchableOpacity} from 'react-native';
 import {NavigationScreenProp} from 'react-navigation';
+import {Calendar} from 'react-native-calendars';
+import {Colors} from '../../assets/colors';
+import {Marking} from '../types';
+import {CALENDAR_TITLE} from '../Constants';
 
 interface Props {
   logout: () => void;
   navigation: NavigationScreenProp<any, any>;
 }
 
-export default class MainView extends Component<Props> {
+interface State {
+  selectedDate: string;
+  markingType: Marking;
+}
+
+export default class MainView extends Component<Props, State> {
+  constructor(props: Props) {
+    super(props);
+    this.state = {
+      selectedDate: '',
+      markingType: Marking.SIMPLE // simple/period
+    };
+    this.toggleSelectedDay = this.toggleSelectedDay.bind(this);
+  }
+
   static navigationOptions = {
     drawerLabel: 'MainView' // TODO change this. Home? Reservations?
   };
 
+  toggleSelectedDay(day) {
+    if (this.state.selectedDate === day.dateString) {
+      this.setState({selectedDate: ''});
+    } else {
+      this.setState({selectedDate: day.dateString});
+    }
+  }
+
   render() {
     return (
       <View style={styles.container}>
-        <Text>Hello World!</Text>
-        <Text>Parkdude mobile client is running succesfully!</Text>
+        <Text style={styles.title}>{CALENDAR_TITLE}</Text>
+        <Calendar
+          markingType={this.state.markingType}
+          onDayPress={(day) => {
+            this.toggleSelectedDay(day);
+          }}
+          minDate={new Date().toISOString().slice(0, 10)}
+          markedDates={{
+            [this.state.selectedDate]: {selected: true, selectedColor: Colors.YELLOW}
+          }}
+          firstDay={1}
+          style={styles.calendar}/>
       </View>
     );
   }
@@ -33,5 +69,18 @@ const styles = StyleSheet.create({
     margin: 10,
     padding: 10,
     backgroundColor: '#DDD'
+  },
+  calendar: {
+    alignSelf: 'stretch',
+  },
+  title: {
+    width: 299,
+    height: 42,
+    // fontFamily: "Exo2",
+    fontSize: 34.8,
+    fontWeight: 'bold',
+    fontStyle: 'normal',
+    letterSpacing: 0,
+    textAlign: 'center',
   }
 });
