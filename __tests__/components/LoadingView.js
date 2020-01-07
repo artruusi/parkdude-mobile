f88f1700be87f3fb10357435c85e029713b3d458
Sami Aalto <sami.aalto@tuni.fi>
2020-01-08 00:30:21 +0200

    Fix failing LoadingView test

--- __tests__/components/LoadingView.js
+++ __tests__/components/LoadingView.js
@@ -1,12 +1,21 @@
 import 'react-native';
+import configureStore from 'redux-mock-store';
 import React from 'react';
 import LoadingView from '../../src/components/LoadingView';
 
 // Note: test renderer must be required after react-native.
 import renderer from 'react-test-renderer';
+import {Provider} from 'react-redux';
+
+const mockStore = configureStore([]);
+const store = mockStore({error: {networkError: ''}});
 
 describe('LoadingView tests', () => {
   it('renders correctly', () => {
-    renderer.create(<LoadingView />);
+    renderer.create(
+      <Provider store={store}>
+        <LoadingView />
+      </Provider>
+    );
   });
 });
