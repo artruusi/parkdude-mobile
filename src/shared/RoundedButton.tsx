142a62ec81f9f561aeea1cd384def193dfb3fc69
Sami Aalto <sami.aalto@tuni.fi>
2020-01-12 16:26:20 +0200

    Add isLoading property to RoundedButton

--- src/shared/RoundedButton.tsx
+++ src/shared/RoundedButton.tsx
@@ -1,10 +1,11 @@
 import React, {Component} from 'react';
-import {StyleSheet, TouchableOpacity, Text, ViewStyle, TextStyle, StyleProp} from 'react-native';
+import {StyleSheet, TouchableOpacity, Text, ViewStyle, TextStyle, StyleProp, ActivityIndicator} from 'react-native';
 import {Colors} from '../../assets/colors';
 
 export interface RoundedButtonProps {
     onPress: () => void;
     disabled?: boolean;
+    isLoading?: boolean;
     buttonText: string;
     buttonStyle?: StyleProp<ViewStyle>;
     textStyle?: StyleProp<TextStyle>;
@@ -16,9 +17,25 @@ export class RoundedButton extends Component<RoundedButtonProps> {
   }
 
   render() {
+    if (this.props.isLoading) {
     return (
       <TouchableOpacity
-        style={[styles.button, (this.props.disabled ? styles.disabledButton : {}), this.props.buttonStyle]}
+          style={[
+            styles.button,
+            this.props.buttonStyle
+          ]}
+          disabled={true}>
+          <ActivityIndicator size="small" color="#000000" />
+        </TouchableOpacity>
+      );
+    }
+    return (
+      <TouchableOpacity
+        style={[
+          styles.button,
+          (this.props.disabled ? styles.disabledButton : {}),
+          this.props.buttonStyle
+        ]}
         disabled={this.props.disabled}
         onPress={this.props.onPress}
         activeOpacity={this.props.disabled ? 1 : 0.7}>
