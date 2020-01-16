22f1d6ebde60f22e0141b7db8863612aa86608c9
Arttu Ruusiala <arttu.ruusiala@tuni.fi>
2020-01-16 11:18:39 +0200

    Fix broken onboarding rendering test

--- __tests__/components/OnboardingView.js
+++ __tests__/components/OnboardingView.js
@@ -5,6 +5,15 @@ import OnboardingView from '../../src/components/OnboardingView';
 // Note: test renderer must be required after react-native.
 import renderer from 'react-test-renderer';
 
+const createMockProps = (props: Record<string, any>) => ({
+  navigation: {
+    navigate: jest.fn(),
+    addListener: jest.fn()
+  },
+  ...props
+});
+
 it('renders correctly', () => {
-  renderer.create(<OnboardingView />);
+  const props: any = createMockProps({});
+  renderer.create(<OnboardingView {...props}/>);
 });
