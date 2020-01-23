125bdd45218c65b84c036e09e5725c4d9acb6715
aatosm <aatosm@hotmail.com>
2020-01-23 15:40:54 +0200

    Fixed error on errorMessage causing infinite setState loop

--- src/components/MainView.tsx
+++ src/components/MainView.tsx
@@ -52,8 +52,10 @@ class MainView extends Component<Props, State> {
     if (!this.props.auth.isAuthenticated) {
       this.props.navigation.navigate('Auth');
     }
-    if (prevProps.error.postReservationError.message !== '') {
-      this.toggleErrorModal();
+    if (prevProps.error.postReservationError.message !== this.props.error.postReservationError.message) {
+      if (this.props.error.postReservationError.message !== '') {
+        this.toggleErrorModal();
+      }
     }
   }
 
